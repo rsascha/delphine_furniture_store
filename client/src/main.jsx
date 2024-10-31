4f36d5ae43da4df9bb8db1b4ba8de4a874b89577
teleidoskopbild <teleidoskopbild@gmail.com>
2024-10-31 12:58:32 +0100

    add login-logout with auth0

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-cztbcp64sc3nnull.us.auth0.com"
+      clientId="iuHBt2vkm74iNSIstyIdLG1gWJOvDu4B"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
