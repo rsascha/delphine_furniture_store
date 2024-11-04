447cd9327ab0f3141e87f030ba4256bd63f637a8
Sascha Rose <sascha.rose@gmail.com>
2024-11-04 14:55:43 +0100

    disable auth

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 import ProductCard from "../components/ProductCard.jsx";
 import "./Products.css";
 
 function Products() {
   const [products, setProducts] = useState([]);
-  const { getAccessTokenSilently } = useAuth0();
+  // const { getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     async function fetchProducts() {
       try {
-        const accessToken = await getAccessTokenSilently().catch((error) => {
-          console.warn("Could not get access token:", error);
-          return null;
-        });
+        // const accessToken = await getAccessTokenSilently().catch((error) => {
+        //   console.warn("Could not get access token:", error);
+        //   return null;
+        // });
 
         const response = await fetch("http://localhost:3000/products", {
-          headers: {
-            Authorization: accessToken ? `Bearer ${accessToken}` : undefined,
-          },
+          // headers: {
+          //   Authorization: accessToken ? `Bearer ${accessToken}` : undefined,
+          // },
         });
 
         if (!response.ok) throw new Error("Network response was not ok");
@@ -31,7 +31,8 @@ function Products() {
     }
 
     fetchProducts(); // Trigger the fetch on component load
-  }, [getAccessTokenSilently]); // Include getAccessTokenSilently in the dependency array
+  }, []);
+  // }, [getAccessTokenSilently]); // Include getAccessTokenSilently in the dependency array
 
   return (
     <div>
--- server/src/app.js
+++ server/src/app.js
@@ -2,28 +2,34 @@ import cors from "cors";
 import express, { json } from "express";
 import productRoute from "./routes/products.js";
 import categoryRoutes from "./routes/categories.js";
-import { auth } from "express-oauth2-jwt-bearer";
-import { config } from "./config.js";
+// import { auth } from "express-oauth2-jwt-bearer";
+// import { config } from "./config.js";
 import { addUserInfo, addUserId } from "./middlewares/index.js";
 import Product from "./models/Product.js";
 import { db } from "./util/db-connect.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const jwtCheck = auth(config.authOptions);
+// const jwtCheck = auth(config.authOptions);
 
 app.use(cors());
 app.use(json());
-app.use(jwtCheck);
+// app.use(jwtCheck);
 
-app.get("/", addUserId, addUserInfo, async (req, res) => {
-  const { userId, userInfo } = req;
-  console.debug({ userId, userInfo });
+// app.get("/", addUserId, addUserInfo, async (req, res) => {
+//   const { userId, userInfo } = req;
+//   console.debug({ userId, userInfo });
+//   res.json({ successfull: true });
+// });
+
+app.get("/", async (req, res) => {
   res.json({ successfull: true });
 });
 
 app.use("/products", productRoute);
 app.use("/", categoryRoutes);
+// Sample to restrict access to the following route
+// app.use("/categories", jwtCheck, categoryRoutes);
 
 app.listen(PORT, () => {
   console.log("api running on port " + PORT);
