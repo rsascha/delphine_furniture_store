0df9c0adc24e95512147bcb1a4cfdfece69a7268
salehAlkarabubi <karabubi66@yahoo.com>
2024-11-04 12:22:43 +0100

    create markup productsDetails

--- server/src/app.js
+++ server/src/app.js
@@ -19,9 +19,8 @@ app.use(jwtCheck);
 app.get("/", addUserId, addUserInfo, async (req, res) => {
   const { userId, userInfo } = req;
   console.debug({ userId, userInfo });
-  await db.connect();
-  const products = await Product.find().populate("categoryId");
-  res.json(products);
+  
+  res.json({successfull: true});
 });
 
 app.use("/", productRoute);
