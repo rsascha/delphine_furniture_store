c393a05412b9e5f408ac45054bd31c227702367b
Ladan-Mirzaei <161485911+Ladan-Mirzaei@users.noreply.github.com>
2024-11-04 12:02:04 +0100

    schema-versionkey

--- server/src/models/Cart.js
+++ server/src/models/Cart.js
@@ -1,16 +1,21 @@
 import { model, Schema } from "mongoose";
 
-const cartSchema = new Schema({
-  products: [
-    {
-      productId: {
-        type: Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
+const cartSchema = new Schema(
+  {
+    products: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        date: { type: Date, default: Date.now },
       },
-      date: { type: Date, default: Date.now },
-    },
-  ],
-});
+    ],
+  },
+  {
+    versionKey: false,
+  }
+);
 export const Cart = model("Cart", cartSchema);
 export default Cart;
