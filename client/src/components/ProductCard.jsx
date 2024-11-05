9df5d08abe1676b17a1fd49a1cca75dc994a438d
salehAlkarabubi <karabubi66@yahoo.com>
2024-11-05 11:15:28 +0100

    Design ProductDetails-page

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -2,12 +2,13 @@ import "./ProductCard.css";
 import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
+  console.log(`/products/${product._id}`);
   return (
     <div className="product-card">
       <h2>{product.name}</h2>
       <p>Price: ${product.price}</p>
       <p>Category: {product.categoryId?.name || "N/A"}</p>
-      <Link to={`/products/${product._id}`}>See more</Link>
+      <Link to={`${product._id}`}>See more</Link>
     </div>
   );
 };
