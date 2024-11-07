4b3be018b6c88eeda8723cb15f63a97ac37aadb3
Ralf Siewert <ralf@actyvy.st>
2024-11-07 10:32:01 +0100

    reset cartCount on logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      setCartCount(0);
+    } else {
+      // TODO: get cartCount for logged in user from server
+    }
+  }, [isAuthenticated]);
 
   function changeCartCount(newCartCount) {
     setCartCount(newCartCount);
